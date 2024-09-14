a219538b4f707a8a0b4f58989dd1312c4a4f8851
Zoilo Granda <zgranda@gmail.com>
2024-09-14 13:00:01 -0400

    update deploy script

--- hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
+++ hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
@@ -1,14 +1,25 @@
-const { networkConfig } = require("../helper-hardhat-config")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { network } = require("hardhat")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
- const { deply, log } = deployments
+ const { deploy, log } = deployments
  const { deployer } = await getNamedAccounts()
  const chainId = network.config.chainId
- const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
- const fundMe = await deployer("FundMe", {
+ let ethUsdPriceFeedAddress
+ if (chainId == 31337) {
+  const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+  ethUsdPriceFeedAddress = ethUsdAggregator.address
+ } else {
+  ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+ }
+ log("----------------------------------------------------")
+ log("Deploying FundMe and waiting for confirmations...")
+ const fundMe = await deploy("FundMe", {
   from: deployer,
-  args: [],
+  args: [ethUsdPriceFeedAddress],
   log: true
  })
+ log("-------------------")
 }
+
+module.exports.tags = ["all", "fundme"]
